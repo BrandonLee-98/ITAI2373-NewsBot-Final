8d8be789057cf20793befaaa491bd42a8e1f89bc
Brandon Lee <bleestudent98@gmail.com>
2026-04-28 17:13:12 -0500

    Add configuration settings for NLP thresholds and paths

---
+++ config/settings.py
@@ -0,0 +1,4 @@
+# Configuration management for NLP thresholds and paths
+SENTIMENT_THRESHOLD = 0.05
+TOPIC_MODEL_METHOD = 'lda'  # Options: 'lda', 'nmf' [cite: 53]
+MODEL_STORAGE_PATH = 'data/models/'
