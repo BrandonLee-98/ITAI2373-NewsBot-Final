7b1343dddd6ff4cec4295fc03c5cff94c0172939
Brandon Lee <bleestudent98@gmail.com>
2026-04-29 08:38:58 -0500

    Fix import path for TopicModeler

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,4 +1,4 @@
 from .classifier import NewsClassifier
 from .sentiment_analyzer import SentimentAnalyzer
-from .analysis.topic_modeler import TopicModeler
+from .topic_modeler import TopicModeler
 from .ner_extractor import EntityRelationshipMapper
